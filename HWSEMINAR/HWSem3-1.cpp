dda42726ae69b1b3d7e1d2849b1a28b72dffd490
Вячеслав Синёв <111271398+err235wqw@users.noreply.github.com>
2022-11-17 19:21:52 +0400

    Create HWSem3-1.cpp

---
+++ HWSEMINAR/HWSem3-1.cpp
@@ -0,0 +1,21 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <windows.h>
+#include <iomanip>
+#include <set>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	long double s;
+	cin >> s;
+	cout << sqrt(s/sqrt(0.1875));
+
+}
