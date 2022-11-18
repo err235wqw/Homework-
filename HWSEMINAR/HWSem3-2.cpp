40944bcf7d3841faa9930866c39734fed331e20e
Вячеслав Синёв <111271398+err235wqw@users.noreply.github.com>
2022-11-18 09:20:53 +0400

    Update HWSem3-2.cpp

--- HWSEMINAR/HWSem3-2.cpp
+++ HWSEMINAR/HWSem3-2.cpp
@@ -12,14 +12,14 @@
 using namespace std;
 const long double g = 6.67430 * 1e-11;
 double aa(double m1, double m2, double r) {
-	return g * (m1*m2) / r;
+	return g * (m1 * m2) / (r*r);
 }
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(0);
-	long double m1,m2,m3,r1,r2,r3;
+	long double m1, m2, m3, r1, r2, r3;
 	cin >> m1 >> m2 >> m3 >> r1 >> r2 >> r3;
-	cout << max(aa(m1,m2,r1),aa(m2,m3,r2),aa(m1,m3,r3));
+	cout <<setprecision(15) << fixed<<max(aa(m1, m2, r1), aa(m2, m3, r2)) << ' ' << max(min(aa(m1, m2, r1), aa(m2, m3, r2)), aa(m1, m3, r3));
 
 }
