4f7e50772af38a56cb6f737272387980ed64bd7f
Вячеслав Синёв <111271398+err235wqw@users.noreply.github.com>
2022-12-22 23:43:50 +0400

    Update 8-1-5-3.cpp

--- HWPRACTICE/8-1-5-3.cpp
+++ HWPRACTICE/8-1-5-3.cpp
@@ -14,41 +14,23 @@
 #include <Windows.h>
 #include <fstream>
 #include <array>
-const long double eps = 1e-12;
 using namespace std;
-template <typename X> void PrintArr( X **a, int n, int m) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++)
-			cout << a[i][j] << ' ';
-		cout << '\n';
+int f(int a,int b) {
+	int c = 0;
+	while (b != 0) {
+		c = b;
+		b = a % c;
+		a = c;
 	}
-}
-template <typename X> void SinArr(X ** a,int n,int m) {
-	for (int i = 0; i < n; i++) {
-		a[i] = new int[m];
-		for (int j = 0; j < m; j++)
-			cin >> a[i][j];
-
-	}
-}
-template <typename X> void FuncArr(X** a, int n, int m, int a1, int b1) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++)
-			if (a[i][j] > a1 && a[i][j] < b1)
-				a[i][j] = 0;
-
-	}
+	return a;
 }
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	cin.tie(0);
 	ios_base::sync_with_stdio(0);;
-	int n, m,a1,b1;
-	cin >> n >> m >> a1 >> b1;
-	int** a = new int *[n];
-	SinArr(a, n, m);
-	FuncArr(a, n, m, a1, b1);
-	PrintArr(a,n,m);
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << (a * d + b * c) / f(a * d + b * c, b * d) << '/' << (d * b) / f(a * d + b * c, d * b);
 	return 0;
 }
